2e3e8125d546469b84069fa33cb53d3020f4dd08
Darya Shnipova <dshnipova@gmail.com>
2023-10-21 16:25:10 +0200

    Add 404 HTML page

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+import path, { dirname } from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
 import adminRoutes from './routes/admin.js';
 import shopRoutes from './routes/shop.js';
+import { fileURLToPath } from 'url';
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
 
 const app = express();
 
@@ -13,7 +18,7 @@ app.use(shopRoutes);
 app.use('/', (req, res) => {
 	res
 		.status(404)
-		.send('<h1>Page is not found!</h1>');
+		.sendFile(path.join(_dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
