58247c339ef9051a985aa73334d1484bc01835db
Darya Shnipova <dshnipova@gmail.com>
2023-10-21 16:07:23 +0200

    Add shop and add-product HTML pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,12 @@
+import path from 'path';
 import express from 'express';
+import { _dirname } from '../helpers/utils.js';
 
 const router = express.Router();
 
 // /admin/add-product ---> GET
 router.get('/add-product', (req, res, next) => {
-	res.send('<form action="/admin/add-product" method="post"><input' +
-		' type="text" name="product"' +
-		' /><button type="submit">Add product</button></form>')
+	res.sendFile(path.join(_dirname, '../', 'views', 'add-product.html'))
 });
 
 // /admin/add-product ---> POST
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
+import path from 'path';
 import express from 'express';
+import { _dirname } from '../helpers/utils.js';
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	res.send('<h1>Home page</h1>');
+	res.sendFile(path.join(_dirname, '../', 'views', 'shop.html'));
 });
 
 export default router;
