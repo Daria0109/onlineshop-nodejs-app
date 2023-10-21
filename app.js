85b94e339d7dd4fe18e7be0c13d580884f4fea53
Darya Shnipova <dshnipova@gmail.com>
2023-10-21 15:23:53 +0200

    Return 404 response

--- app.js
+++ app.js
@@ -10,4 +10,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use('/', (req, res) => {
+	res
+		.status(404)
+		.send('<h1>Page is not found!</h1>');
+});
+
 app.listen(3000);
