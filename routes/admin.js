b76a0f3597b08c1cc0255cefab5937b30c0ad489
Darya Shnipova <dshnipova@gmail.com>
2023-10-21 15:32:25 +0200

    Handle paths with similar start part

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,15 @@ import express from 'express';
 
 const router = express.Router();
 
+// /admin/add-product ---> GET
 router.get('/add-product', (req, res, next) => {
-	res.send('<form action="/product" method="post"><input' +
+	res.send('<form action="/admin/add-product" method="post"><input' +
 		' type="text" name="product"' +
 		' /><button type="submit">Add product</button></form>')
 });
 
-router.post('/product', (req, res, next) => {
+// /admin/add-product ---> POST
+router.post('/add-product', (req, res, next) => {
 	console.log(req.body);
 	res.redirect('/');
 });
